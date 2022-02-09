2f889180d4ccf3a29b130a4c3ad625ebf7e7e567
Marisela Vasquez <83312462+alesiram@users.noreply.github.com>
2022-02-08 20:18:52 -0800

    Initial Commit

---
+++ Flix2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix2.0
+//
+//  Created by Marisela Vasquez on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
